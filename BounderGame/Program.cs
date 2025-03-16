6cbc0803ac8c9dd157cf91d2a2c1ebdbe5896365
Dawid Wika <wujek.jasirus@gmail.com>
2025-03-16 21:27:05 +0100

    Dodaj pliki projektów.

---
+++ BounderGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BounderGame
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj Strzelcu!");
+            Console.WriteLine("Jak Ci na imię?");
+
+                var shooter = Console.ReadLine();
+
+            Console.WriteLine("No dobra, " + shooter + "!");
+            Console.WriteLine("Pokaż na co Cię stać!");
+        }
+    }
+}
